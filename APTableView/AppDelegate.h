b82b65b008cab23b4c728ae590cca4d056f090a5
Mac <mac@macs-macbook-air.local>
2016-09-13 14:23:00 +0530

    Initial Commit

---
+++ APTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APTableView
+//
+//  Created by Mac on 13/09/16.
+//  Copyright © 2016 Akshay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ APTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  APTableView
+//
+//  Created by Mac on 13/09/16.
+//  Copyright © 2016 Akshay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
